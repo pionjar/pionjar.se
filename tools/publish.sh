e3ccdaae2e56c1b589565a789850e02bf22b1843
Eric Leimar <eric.leimar@vidhance.com>
2017-03-17 10:26:27 +0100

    Ändrade publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 pushd pionjar.se
-git add -A
+git add .
 git commit -m "$1"
 git pull upstream master
-git push origin master
+hugo
+popd
+pushd public
+git push -f
+
+#Kolla så det funkar med att pusha public-mappen manuellt till gh-pages
